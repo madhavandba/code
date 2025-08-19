7110d42299c40f1667a67fd04dafe09266ca81e2
madhavandba <madhavandba@gmail.com>
2025-08-19 08:31:35 +0530

    adding

---
+++ add.sql
@@ -0,0 +1,54 @@
+DELIMITER $$
+
+CREATE PROCEDURE get_all_employees()
+BEGIN
+    -- Select all rows from employees table
+    SELECT * FROM employees;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE get_all_employees1()
+BEGIN
+    -- Select all rows from employees table
+    SELECT * FROM employees;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE get_all_employees3()
+BEGIN
+    -- Select all rows from employees table
+    SELECT * FROM employees;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE get_all_employees2()
+BEGIN
+    -- Select all rows from employees table
+    SELECT * FROM employees;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE get_all_employees4()
+BEGIN
+    -- Select all rows from employees table
+    SELECT * FROM employees;
+END$$
+
+DELIMITER ;
+
